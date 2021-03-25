6b40cac65f77609f43cd97c71c2cd989a3e8e947
Lev Popov <levpopov1@gmail.com>
2021-03-25 18:03:06 +0000

    adds theme data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import './App.css';
+import ColorRow from './components/ColorRow';
+
+const theme = {
+  colors: {
+    primary: {
+      name: "Primary",
+      hex: "#0000ef",
+      rgb: "rgb(0,0,239)",
+      hsl: "hsl(240,100%,20%)"
+    },
+    secondary: {
+      name: "Secondary",
+      hex: "#efef00",
+      rgb: "rgb(239,239,0)",
+      hsl: "hsl(240,100%,20%)"
+    },
+    highlight: {
+      name: "Highlight",
+      hex: "#0078ef",
+      rgb: "rgb(0,120,239)",
+      hsl: "hsl(240,100%,20%)"
+    },
+    shadow: {
+      name: "Shadow",
+      hex: "#000052",
+      rgb: "rgb(0,0,82)",
+      hsl: "hsl(240,100%,20%)"
+    },
+    light: {
+      name: "Light",
+      hex: "#efefff",
+      rgb: "rgb(239,239,255)",
+      hsl: "hsl(240,100%,20%)"
+    },
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App container">
+      <ColorRow theme={theme}/>
     </div>
   );
 }
