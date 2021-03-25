df280150ae96137fc6236c2f5d8cff8c6aad2b5c
Lev Popov <levpopov1@gmail.com>
2021-03-25 15:43:47 +0000

    import bootstrap css and js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css';
 import App from './App';
 
