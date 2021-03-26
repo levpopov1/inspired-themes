d85ef5e15451eeaacacd8e0e78f25ed64f815689
Lev Popov <levpopov1@gmail.com>
2021-03-26 20:02:18 +0000

    adds theme collection component

---
+++ src/components/ThemeCollection.js
@@ -0,0 +1,16 @@
+import Theme from './Theme';
+
+function ThemeCollection({collection}) {
+  return (
+    <div className="dropdown-column col-sm-3">
+      <h6 className="dropdown-header">{collection.name}</h6>
+      {
+        collection.themes.map(theme => 
+          <Theme theme={theme} key={theme.id}/>
+        )
+      }
+    </div>
+  );
+}
+
+export default ThemeCollection;
