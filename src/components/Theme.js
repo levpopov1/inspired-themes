a383cfee2f5ec5e7358d8312e3e68166735fda9d
Lev Popov <levpopov1@gmail.com>
2021-03-26 20:02:25 +0000

    adds theme component

---
+++ src/components/Theme.js
@@ -0,0 +1,14 @@
+import MiniPalette from './MiniPalette';
+
+function Theme({theme}) {
+  return (
+    <li>
+      <a className="dropdown-item d-flex flex-row align-items-center p-3" href="/tokyo-series/pastel">
+        <MiniPalette colors={theme.colors}/>
+        <span className="text-start ps-2 text-wrap">{theme.name}</span>
+      </a>
+    </li>
+  );
+}
+
+export default Theme;
