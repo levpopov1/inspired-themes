1b3637e8fd155c6fc94ff0009bc8d1cc491eae9e
Lev Popov <levpopov1@gmail.com>
2021-03-26 18:48:46 +0000

    changes palette from gradient to blocks

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -1,7 +1,11 @@
 function MiniPalette({colors}) {
   return (
-    <div className="minipallete">
-      <div className="gradient-block" style={{ linearGradient: {from: colors.primary.hex, to: colors.secondary.hex} }}></div>
+    <div className="minipalette">
+      <div className="palette-block me-1" style={{ backgroundColor: colors.primary.hex }}></div>
+      <div className="palette-block me-1" style={{ backgroundColor: colors.secondary.hex }}></div>
+      <div className="palette-block me-1" style={{ backgroundColor: colors.highlight.hex }}></div>
+      <div className="palette-block me-1" style={{ backgroundColor: colors.shadow.hex }}></div>
+      <div className="palette-block me-1" style={{ backgroundColor: colors.light.hex }}></div>
     </div>
   );
 }
