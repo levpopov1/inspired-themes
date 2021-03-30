e94100fa38ac8901ad5133b01fefe01efe6cd9ee
Lev Popov <levpopov1@gmail.com>
2021-03-30 19:27:21 +0100

    adds xxxl row width setting

--- src/components/TypographyRow.js
+++ src/components/TypographyRow.js
@@ -2,7 +2,7 @@ import TypographyBlock from './TypographyBlock'
 
 function TypographyRow({theme}) {
   return (
-    <div className="row mb-3 row-cols-md-2 row-cols-lg-2 row-cols-xl-3">
+    <div className="row mb-3 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxxl-5">
       {Object.keys(theme.colors).map((key, index) => 
         <TypographyBlock color={theme.colors[key]} key={index}/>
       )}
