8ade96db9ade6bc24a50f241d337f5bce7b302af
Lev Popov <levpopov1@gmail.com>
2021-03-31 18:24:15 +0100

    adds more breakpoints to row

--- src/components/ColorRow.js
+++ src/components/ColorRow.js
@@ -2,7 +2,7 @@ import ColorBlock from './ColorBlock'
 
 function ColorRow({theme}) {
   return (
-    <div className="row mb-3 row-cols-md-2 row-cols-lg-2 row-cols-xl-5 row-cols-xxxl-5">
+    <div className="row mb-3 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-4 row-cols-xxl-5">
       {Object.keys(theme.colors).map((key, index) => 
         <ColorBlock color={theme.colors[key]} key={index}/>
       )}
