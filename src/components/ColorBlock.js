73204982a512c318ca22ef41ed60dd20005a1381
Lev Popov <levpopov1@gmail.com>
2021-03-31 18:23:57 +0100

    testing new layout

--- src/components/ColorBlock.js
+++ src/components/ColorBlock.js
@@ -3,32 +3,51 @@ function ColorBlock({color}) {
     <div className="col mb-3">
       <div className="card">
         <div className="row g-0">
-          <div className="col-md-2 col-lg-12">
+          <div className="col-3 col-sm-12 col-xxxl-3">
             <div className="color-block" style={{ backgroundColor: color.hex }}></div>
           </div>
-          <div className="col-md-10 col-lg-12">
+          <div className="col-9 col-sm-12 col-xxxl-9">
             <div className="card-body">
               <h3 className="card-title m-0">{ color.name }</h3>
             </div>
             <div className="card-body">
-              <div className="d-flex justify-content-between mb-2">
+              <p className="card-text">
+                {/* <b className="color-code me-3 d-block">HEX</b>  */}
+                <code className="code css d-block">
+                    { color.hex }
+                </code>
+              </p>
+              <p className="card-text">
+                {/* <b className="color-code me-3 d-block">RGB</b>  */}
+                <code className="code css d-block">
+                    { color.rgb }
+                </code>
+              </p>
+              <p className="card-text">
+                {/* <b className="color-code me-3 d-block">HSL</b>  */}
+                <code className="code css d-block">
+                  { color.hsl }
+                </code>
+              </p>
+
+              {/* <div className="d-flex flex-column mb-2">
                 <b className="color-code me-3 align-self-center">HEX:</b> 
                 <code className="code css flex-grow-1">
                   { color.hex }
                 </code>
               </div>
-              <div className="d-flex justify-content-between mb-2">
+              <div className="d-flex flex-column mb-2">
                 <b className="color-code me-3 align-self-center">RGB:</b> 
                 <code className="code css flex-grow-1">
                   { color.rgb }
                 </code>
               </div>
-              <div className="d-flex justify-content-between mb-2">
+              <div className="d-flex flex-column mb-2">
                 <b className="color-code me-3 align-self-center">HSL:</b> 
                 <code className="code css flex-grow-1">
                   { color.hsl }
                 </code>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
