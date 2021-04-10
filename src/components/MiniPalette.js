a738de9e6b874338a5935cf1a90db2b7df550646
Lev Popov <levpopov1@gmail.com>
2021-04-10 18:20:36 +0100

    renames shadow to dark

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -4,7 +4,7 @@ function MiniPalette({colors}) {
       <div className="palette-block me-1" style={{ backgroundColor: colors.primary.hex }}></div>
       <div className="palette-block me-1" style={{ backgroundColor: colors.secondary.hex }}></div>
       <div className="palette-block me-1" style={{ backgroundColor: colors.highlight.hex }}></div>
-      <div className="palette-block me-1" style={{ backgroundColor: colors.shadow.hex }}></div>
+      <div className="palette-block me-1" style={{ backgroundColor: colors.dark.hex }}></div>
       <div className="palette-block me-1" style={{ backgroundColor: colors.light.hex }}></div>
     </div>
   );
