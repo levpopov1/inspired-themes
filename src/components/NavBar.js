bd7187575573b9b6ebd3a957ea34d0ceb9d7c87e
Lev Popov <levpopov1@gmail.com>
2021-04-10 18:20:20 +0100

    changes responsive classes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,11 @@ import ThemeCollection from './ThemeCollection';
 
 function NavBar({collections}) {
   return (
-    <header className="shadow-sm">
-      <div className="container">
+    <header className="shadow-sm w-100">
+      <div className="container-fluid">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-2">
           <div className="col-md-4 d-flex align-items-center mb-2 mb-md-0">
-            <a href="/" className="d-flex align-items-center text-dark text-decoration-none" id="logo">
+            <a href="/" className="d-flex align-items-center text-decoration-none" id="logo">
               <i className="bi bi-palette2 me-2"/>
               Inspired Themes
             </a>
@@ -14,14 +14,14 @@ function NavBar({collections}) {
           <div className="col-12 col-md-4 justify-content-center mb-2 mb-md-0">
             <form className="w-100 me-3">
               <div className="input-group">
-                <input type="text" className="form-control" placeholder="Paste Twitter URL here" aria-label="Paste Twitter URL here" aria-describedby="button-addon"/>
-                <button className="btn btn-outline-primary" type="button" id="button-addon">Generate</button>
+                <input type="text" className="form-control" placeholder="Paste image URL here" aria-label="Paste image URL here" aria-describedby="button-addon"/>
+                <button className="btn btn-outline-light" type="button" id="button-addon">Generate</button>
               </div>
             </form>
           </div>
           <div className="col-md-4 text-end mb-md-0">
             <div className="dropdown pos-initial">
-              <a href="#" className="link-dark text-decoration-none dropdown-toggle" id="dropdownNavLink" data-bs-toggle="dropdown" aria-expanded="false">
+              <a href="#" className="link text-decoration-none dropdown-toggle" id="dropdownNavLink" data-bs-toggle="dropdown" aria-expanded="false">
                 Theme Gallery
               </a>
               <ul className="dropdown-menu dropdown-menu-horizontal full-width row" aria-labelledby="dropdownNavLink">
