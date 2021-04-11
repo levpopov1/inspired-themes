23a16b8ba8a231726545ba4410591f5325b20e9d
Lev Popov <levpopov1@gmail.com>
2021-04-11 15:57:18 +0100

    improves layout on xxxl screens

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -3,9 +3,9 @@ import MiniPalette from './MiniPalette';
 function Theme({theme}) {
   return (
     <li>
-      <a className="dropdown-item d-flex flex-row align-items-center p-3" href="/tokyo-series/pastel">
+      <a className="dropdown-item d-flex flex-row align-items-center justify-content-between p-3" href="/tokyo-series/pastel">
+        <span className="text-start pe-3 text-wrap">{theme.name}</span>
         <MiniPalette colors={theme.colors}/>
-        <span className="text-start ps-3 text-wrap">{theme.name}</span>
       </a>
     </li>
   );
--- src/components/ThemeCollection.js
+++ src/components/ThemeCollection.js
@@ -2,7 +2,7 @@ import Theme from './Theme';
 
 function ThemeCollection({collection}) {
   return (
-    <div className="dropdown-column col-sm-3">
+    <div className="dropdown-column col-sm-3 col-xxxl-2">
       <h6 className="dropdown-header">{collection.name}</h6>
       {
         collection.themes.map(theme => 
