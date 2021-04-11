50d6039f80d3ec9b36dd3fa17b26d31d1af49bc7
Lev Popov <levpopov1@gmail.com>
2021-04-11 22:58:25 +0100

    repalces link with button

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -3,10 +3,10 @@ import MiniPalette from './MiniPalette';
 function Theme({theme}) {
   return (
     <li>
-      <a className="dropdown-item d-flex flex-row align-items-center justify-content-between p-3" href="/tokyo-series/pastel">
+      <button className="dropdown-item d-flex flex-row align-items-center justify-content-between p-3" >
         <span className="text-start pe-3 text-wrap">{theme.name}</span>
         <MiniPalette colors={theme.colors}/>
-      </a>
+      </button>
     </li>
   );
 }
