13987cf23476e6c4af74783906f45745886f725d
Lev Popov <levpopov1@gmail.com>
2021-04-14 22:18:18 +0100

    creates typographySection component

---
+++ src/components/TypographySection.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import TypographyHeader from './TypographyHeader';
+import TypographyRow from './TypographyRow';
+
+function TypographySection({theme, getContrast}) {
+
+  const [font, setFont] = useState('sans-serif');
+  const [showInfo, setShowInfo] = useState(true);
+
+
+  return (
+    <div className="section-container">
+      <TypographyHeader font={font} setFont={setFont} showInfo={showInfo} setShowInfo={setShowInfo}/>
+      <TypographyRow theme={theme} font={font} getContrast={getContrast} showInfo={showInfo}/>
+    </div>
+  );
+}
+
+export default TypographySection;
