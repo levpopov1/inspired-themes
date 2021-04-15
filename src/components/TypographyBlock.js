5f4120e123678c664c8bf84aef96f3dffc92958d
Lev Popov <levpopov1@gmail.com>
2021-04-15 17:25:23 +0100

    wraps content in card-body

--- src/components/TypographyBlock.js
+++ src/components/TypographyBlock.js
@@ -10,19 +10,23 @@ function TypographyBlock({color, font, getContrast}) {
 
   return (
     <div className={`col mb-3 font-${font}`}>
-      <div className={`card h-100 p-5 text-${contrastColor}`} style={{ backgroundColor: color.hex }}>
-        <h1 className="display-5 fw-bold">Example of Bold Title</h1>
-        <h2 className="lead fs-3">Example of Thin Subtitle</h2>
-        <p className="text mt-auto">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-          Unde eum delectus rem amet nisi necessitatibus debitis, 
-          error reprehenderit dolores sit ducimus ab rerum quod aperiam veritatis? 
-          Voluptates corrupti vero id.
-        </p>
-        <p className="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Asperiores voluptate iusto amet dolore, autem veniam.
-        </p>
+      <div className={`card h-100 p-4 text-${contrastColor}`} style={{ backgroundColor: color.hex }}>
+        <div className="card-body">
+          <h1 className="display-5 fw-bold">Example of Bold Title</h1>
+          <h2 className="lead fs-3">Example of Thin Subtitle</h2>
+        </div>
+        <div className="card-body d-flex flex-column justify-content-end">
+          <p className="text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Unde eum delectus rem amet nisi necessitatibus debitis, 
+            error reprehenderit dolores sit ducimus ab rerum quod aperiam veritatis? 
+            Voluptates corrupti vero id.
+          </p>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Asperiores voluptate iusto amet dolore, autem veniam.
+          </p>
+        </div>
       </div>
     </div>
   );
