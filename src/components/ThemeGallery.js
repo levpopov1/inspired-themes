f1a2d0ad7a44ba12eba431a127f71fd681b43f17
Lev Popov <levpopov1@gmail.com>
2021-04-16 03:54:13 +0100

    adds themechange and transition function

--- src/components/ThemeGallery.js
+++ src/components/ThemeGallery.js
@@ -1,11 +1,23 @@
 import ThemeCollection from "./ThemeCollection";
 import Theme from "./Theme";
 
-function ThemeGallery({collections}) {
+function ThemeGallery({collections, setTheme}) {
+
+  const openOverviweSection = () => {
+    let currentSection = document.querySelector('#vac #s00');
+    let overviewSection = document.querySelector('#vac #s0');
+    currentSection.parentNode.classList.remove("open");
+    currentSection.classList.remove("show");
+    overviewSection.parentNode.classList.add("open");
+    overviewSection.classList.add("show");
+  }
   
-  // const handleThemeChange = () => {
-  //   setThemeID(theme.id);
-  // }
+  const handleThemeChange = (id, collectionName) => {
+    let currentCollection = collections.find(cl => cl.name === collectionName);
+    let nextTheme = currentCollection.themes.find(item => item.id === id);
+    setTheme(nextTheme);
+    openOverviweSection();
+  }
 
   return (
     <div className="section-container py-3">
@@ -13,7 +25,7 @@ function ThemeGallery({collections}) {
         {collections.map(collection =>
           // <ThemeCollection collection={collection} key={collection.id}/>
           collection.themes.map(theme => 
-            <Theme theme={theme} key={theme.id}/>
+            <Theme theme={theme} setTheme={handleThemeChange} key={theme.id}/>
           )
         )}
       </div>
