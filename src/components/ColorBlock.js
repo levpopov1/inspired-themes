d92a10f8b26bbce122565aaa0b017aee9e309091
Lev Popov <levpopov1@gmail.com>
2021-04-16 23:23:00 +0100

    adds placeholder elements

--- src/components/ColorBlock.js
+++ src/components/ColorBlock.js
@@ -1,7 +1,17 @@
 function ColorBlock({color}) {
   return (
-    <div className="col mb-3">
-      some text
+    <div className="col-sm-2">
+      <div className="card my-3">
+        <div className="card-body">
+          <h5 className="card-title">{color.name}</h5>
+          <h5 className="card-title">Contrast Ratio</h5>
+          <p className="card-text">
+            Do not use this color combo. 
+            because the ratio is lower than 7:1 
+            so it will have poor visibility.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
