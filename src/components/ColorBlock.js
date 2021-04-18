3d72147ad020d5286f4dbc2b62e9b0df0b23156d
Lev Popov <levpopov1@gmail.com>
2021-04-18 01:13:38 +0100

    implements gauge and text setup

--- src/components/ColorBlock.js
+++ src/components/ColorBlock.js
@@ -1,14 +1,32 @@
-function ColorBlock({color}) {
+import ContrastRatioGauge from "./ContrastRatioGauge";
+
+function ColorBlock({color, contrastRatio, rowColor}) {
+
+  let advice = "";
+
+  if(contrastRatio >=12){
+    advice = `${color.name} and ${rowColor.name} is the best pairing.`;
+  }
+  else if(contrastRatio >= 7){
+    advice = `${color.name} works well as a foreground color on a ${rowColor.name} color background.`;
+  }
+  else if(contrastRatio >= 3.5){
+    advice = `${color.name} has a low contrast ratio on a ${rowColor.name} color background. Only use this for fonts larger than 24px or large block elements.`;
+  }
+  else{
+    advice = `${color.name} is almost invisible on a ${rowColor.name} color background. Do not use this pairing.`;
+  }
+
   return (
-    <div className="col-sm-2">
-      <div className="card my-3">
+    <div className="col-md-6 col-xl-3 col-xxxl-2 mb-3">
+      <div className="card h-100" style={{ border: `1px solid ${color.hex}`, backgroundColor: "transparent", color: color.hex }}>
+        <div className="card-header">
+          <ContrastRatioGauge value={contrastRatio}/>
+        </div>
         <div className="card-body">
           <h5 className="card-title">{color.name}</h5>
-          <h5 className="card-title">Contrast Ratio</h5>
           <p className="card-text">
-            Do not use this color combo. 
-            because the ratio is lower than 7:1 
-            so it will have poor visibility.
+            {advice}
           </p>
         </div>
       </div>
