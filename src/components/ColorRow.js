04be3509d270e9055032aa034281cf1bc365a8ff
Lev Popov <levpopov1@gmail.com>
2021-04-18 01:12:43 +0100

    adds contrastRatio prop

--- src/components/ColorRow.js
+++ src/components/ColorRow.js
@@ -1,16 +1,15 @@
 import ColorBlock from './ColorBlock';
 
-function ColorRow({color, theme, contrastRatio}) {
+function ColorRow({color, theme, getContrastRatio}) {
   return (
-    <div className="row" style={{ backgroundColor: color.hex }}>
-      <div className="col-sm-4">
-        <h2 className="title">{color.name}</h2>
+    <div className="row justify-content-end" style={{ backgroundColor: color.hex }}>
+      <div className="col-sm-12">
+        <h2 className="title my-3">{color.name}</h2>
       </div>
       {Object.keys(theme.colors).map(function(colorKey, index){
         if(colorKey !== color.name.toLowerCase()){
-          return (
-            <ColorBlock key={index} color={theme.colors[colorKey]}/>
-          )
+          let contrastRatio = getContrastRatio(color.hex, theme.colors[colorKey].hex);
+          return <ColorBlock key={index} color={theme.colors[colorKey]} contrastRatio={contrastRatio} rowColor={color}/>
         }
       })}
     </div>
