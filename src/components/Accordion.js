5c5d01fb6c510011a8f668086ec86512fa9dcbdc
Lev Popov <levpopov1@gmail.com>
2021-04-23 20:02:58 +0100

    extracts getContrast to lib function

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,7 +3,7 @@ import ColorSection from './ColorSection';
 import MegaPalette from "./MegaPalette";
 import ThemeGallery from './ThemeGallery';
 import TypographySection from './TypographySection';
-
+import getContrast from '../lib/getContrast';
 
 const toggleDisplayState = (e) => {
   setTimeout(() => {
@@ -43,31 +43,6 @@ const getAncestorByClassName = (el, target) => {
 }
 
 function Accordion({theme, collections, setTheme}) {
-
-  const getContrast = (hexcolor) => {
-
-    // default to light if no color is provided
-    if(!hexcolor){
-      return 'light';
-    }
-
-    // If a leading # is provided, remove it
-    if (hexcolor.slice(0, 1) === '#') {
-      hexcolor = hexcolor.slice(1);
-    }
-  
-    // Convert to RGB value
-    let r = parseInt(hexcolor.substr(0,2),16);
-    let g = parseInt(hexcolor.substr(2,2),16);
-    let b = parseInt(hexcolor.substr(4,2),16);
-  
-    // Get YIQ ratio
-    let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-  
-    // Check contrast
-    return (yiq >= 128) ? 'dark' : 'light';
-  
-  };
 
   const [contrastColor, setContrastColor] = useState("dark");
 
