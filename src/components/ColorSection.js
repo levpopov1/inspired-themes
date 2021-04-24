17de8e4c7d6053499261090d3925c8ee5dbd9c3e
Lev Popov <levpopov1@gmail.com>
2021-04-24 20:51:35 +0100

    moves contrast ration function to lib

--- src/components/ColorSection.js
+++ src/components/ColorSection.js
@@ -1,82 +1,56 @@
 import ColorRow from './ColorRow';
 
 function ColorSection({theme}) {
-
-  const getContrastRatio = (color1, color2) => {
-
-    // check we have both colors
-    if(!color1 || !color1){
-      return null;
-    }
-
-    // If a leading # is provided, remove it
-    if (color1.slice(0, 1) === '#') {
-      color1 = color1.slice(1);
-    }
-
-    // If a leading # is provided, remove it
-    if (color2.slice(0, 1) === '#') {
-      color2 = color2.slice(1);
-    }
-
-    // Convert color1 from hex to RGB value
-    let r1 = parseInt(color1.substr(0,2),16);
-    let g1 = parseInt(color1.substr(2,2),16);
-    let b1 = parseInt(color1.substr(4,2),16);
-
-    // Convert color2 from hex to RGB value
-    let r2 = parseInt(color2.substr(0,2),16);
-    let g2 = parseInt(color2.substr(2,2),16);
-    let b2 = parseInt(color2.substr(4,2),16);
-
-    // Convert color1 to SRGB
-    let SRGB_r1 = r1 / 255;
-    let SRGB_g1 = g1 / 255;
-    let SRGB_b1 = b1 / 255;
-
-    // calculate final color1 values for comparison
-    let c1_r = SRGB_r1 <= 0.03928 ? SRGB_r1 / 12.92 : Math.pow(((SRGB_r1 + 0.055) / 1.055), 2.4);
-    let c1_g = SRGB_g1 <= 0.03928 ? SRGB_g1 / 12.92 : Math.pow(((SRGB_g1 + 0.055) / 1.055), 2.4);
-    let c1_b = SRGB_b1 <= 0.03928 ? SRGB_b1 / 12.92 : Math.pow(((SRGB_b1 + 0.055) / 1.055), 2.4);
-
-    // Convert color2 to SRGB
-    let SRGB_r2 = r2 / 255;
-    let SRGB_g2 = g2 / 255;
-    let SRGB_b2 = b2 / 255;
-
-    // calculate final color1 values for comparison
-    let c2_r = SRGB_r2 <= 0.03928 ? SRGB_r2 / 12.92 : Math.pow(((SRGB_r2 + 0.055) / 1.055), 2.4);
-    let c2_g = SRGB_g2 <= 0.03928 ? SRGB_g2 / 12.92 : Math.pow(((SRGB_g2 + 0.055) / 1.055), 2.4);
-    let c2_b = SRGB_b2 <= 0.03928 ? SRGB_b2 / 12.92 : Math.pow(((SRGB_b2 + 0.055) / 1.055), 2.4);
-
-    // calculate luminance value for both colors
-    let l1 = 0.2126 * c1_r + 0.7152 * c1_g + 0.0722 * c1_b;
-    let l2 = 0.2126 * c2_r + 0.7152 * c2_g + 0.0722 * c2_b;
-
-    // Calculate contrast ratio of the two colors
-    let cr = (Math.max(l1, l2) + 0.05) / (Math.min(l1, l2) + 0.05);
-
-    // round the number to 2 decimal places
-    return Math.round(cr * 100) / 100;
-  }
-
   return (
     <div className="section-container">
       <div className="row">
-        <div className="col-sm-6">
+        <div className="col-sm-8 col-md-6 col-xxxl-4 col-uhd-3">
           <h2 className="title my-3">Pairings</h2>
           <p className="text">
-            Write a description about how to use color pairings below and what contrast values mean.
+            You can combine colors in your theme to make pairings that will form the basis for your design.
+            However, while some colors in your theme will pair together really well, others will not.
           </p>
           <p className="text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Laboriosam vel magnam quod asperiores dicta nam tempore esse praesentium enim? 
-            Odio officia totam cumque, dignissimos necessitatibus aut reiciendis debitis harum numquam!
+            The matrix below shows all possible color pairs in your chosen theme.
+            Each pair is given a score between 1 on the low end, and 21 on the high end. 
+            This score is the contrast ratio between the two colors, as defined in the
+            Web Content Accessibility Guidelines (WCAG) documentation. 
+            The higher the score, the better those colors go together.
           </p>
+          <p className="text">
+            You should always aim to use pairings that have a score of 7 or greater, 
+            as that is the WCAG AAA standard of good contrast ratio for normal sized text.  
+          </p>
+          <p className="text">
+            A score above 4.5 
+          </p>
+          <p className="text">
+            Scores between 3 and 7 are not as good, but can still be used in some cases.
+            A contrast ratio of 3 is the absolute minimum required to meet the WCAG AA standard.
+            Do keep in mind though, that people with poor vision or some color blindesses 
+            will struggle to read text in this range, so make sure to use a large font, ideally 24px or greater.
+          </p>
+          <p className="text">
+            A contrast ratio of less than 3 is considered a fail by WCAG and should therefore NEVER be used.
+            Even people with perfect vision will struggle to read text with such a low contrast.
+          </p>
+        </div>
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Score of 7 or greater</h5>
+              <p className="card-text">
+                WCAG AAA : PASS
+              </p>
+              <p className="card-text">
+                Good for normal sized text
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       {Object.keys(theme.colors).map((colorKey, index) =>  
-        <ColorRow color={theme.colors[colorKey]} theme={theme} getContrastRatio={getContrastRatio} key={index}/>
+        <ColorRow color={theme.colors[colorKey]} theme={theme} key={index}/>
       )}
     </div>
   );
