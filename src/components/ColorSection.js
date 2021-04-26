1563be2436675448dcb874061a2217d3a8f70058
Lev Popov <levpopov1@gmail.com>
2021-04-26 20:24:00 +0100

    updates description text

--- src/components/ColorSection.js
+++ src/components/ColorSection.js
@@ -13,40 +13,32 @@ function ColorSection({theme}) {
           <p className="text">
             The matrix below shows all possible color pairs in your chosen theme.
             Each pair is given a score between 1 on the low end, and 21 on the high end. 
-            This score is the contrast ratio between the two colors, as defined in the
-            Web Content Accessibility Guidelines (WCAG) documentation. 
-            The higher the score, the better those colors go together.
+            This score is the contrast ratio of the two colors, and is given a rating of AAA or AA
+            as defined in the Web Content Accessibility Guidelines (WCAG) documentation. 
           </p>
           <p className="text">
-            You should always aim to use pairings that have a score of 7 or greater, 
-            as that is the WCAG AAA standard of good contrast ratio for normal sized text.  
+            You should always aim to use color pairs that pass the WCAG AAA rating to provide the best user experience.
+            The scoring criteria is described below but put simply, the higher the score, the better those colors go together.
           </p>
           <p className="text">
-            A score above 4.5 
+            The AAA rating is achieved when the contrast ratio is greater than 7 for normal sized text (font-size: 16px or 1em) 
+            and greater than 4.5 for large text (font-size: 24px or 1.5em).
           </p>
           <p className="text">
-            Scores between 3 and 7 are not as good, but can still be used in some cases.
-            A contrast ratio of 3 is the absolute minimum required to meet the WCAG AA standard.
-            Do keep in mind though, that people with poor vision or some color blindesses 
-            will struggle to read text in this range, so make sure to use a large font, ideally 24px or greater.
+            The AA rating is achieved when the contrast ratio is greater than 4.5 for normal sized text (font-size: 16px or 1em)
+            and greater than 3 for large text (font-size: 24px or 1.5em).
           </p>
           <p className="text">
             A contrast ratio of less than 3 is considered a fail by WCAG and should therefore NEVER be used.
-            Even people with perfect vision will struggle to read text with such a low contrast.
+            Even people with perfect vision will struggle to read text with such low contrast.
           </p>
-        </div>
-        <div className="col-sm-6">
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Score of 7 or greater</h5>
-              <p className="card-text">
-                WCAG AAA : PASS
-              </p>
-              <p className="card-text">
-                Good for normal sized text
-              </p>
-            </div>
-          </div>
+          <p className="text">
+            To summarise, color pairs with the AA rating are not as good as those that pass AAA,
+            but can still be used in some cases.
+            Do keep in mind though, that people with poor vision or some color blindesses 
+            will struggle to read text in the AA range, so if you choose to use these colors
+            make sure to use a large font, ideally 24px or greater.
+          </p>
         </div>
       </div>
       {Object.keys(theme.colors).map((colorKey, index) =>  
