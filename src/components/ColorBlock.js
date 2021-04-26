ef1af1e953d8593a88e589866099d6a182005acb
Lev Popov <levpopov1@gmail.com>
2021-04-26 20:24:45 +0100

    changes layout and styling

--- src/components/ColorBlock.js
+++ src/components/ColorBlock.js
@@ -5,9 +5,9 @@ import getContrastRatio from '../lib/getContrastRatio';
 
 function ColorBlock({color, rowColor}) {
 
-  const [contrastColor, setContrastColor] = useState('light');
-  const [wcagAA, setWcagAA] = useState("FAIL");
-  const [wcagAAA, setWcagAAA] = useState("FAIL");
+  const [contrastColor, setContrastColor] = useState('');
+  const [wcagAA, setWcagAA] = useState(false);
+  const [wcagAAA, setWcagAAA] = useState(false);
 
   let contrastRatio = getContrastRatio(rowColor.hex, color.hex);
 
@@ -17,20 +17,20 @@ function ColorBlock({color, rowColor}) {
 
   useEffect(() => {
     if(contrastRatio >= 7){
-      setWcagAA("PASS");
-      setWcagAAA("PASS");
+      setWcagAA(true);
+      setWcagAAA(true);
     }
     else if(contrastRatio >= 3.5){
-      setWcagAA("PASS");
-      setWcagAAA("FAIL");
+      setWcagAA(true);
+      setWcagAAA(false);
     }
     else{
-      setWcagAA("FAIL");
-      setWcagAAA("FAIL");
+      setWcagAA(false);
+      setWcagAAA(false);
     }
     return () => {
-      setWcagAA("FAIL");
-      setWcagAAA("FAIL");
+      setWcagAA(false);
+      setWcagAAA(false);
     }
   }, [contrastRatio]);
 
@@ -44,37 +44,36 @@ function ColorBlock({color, rowColor}) {
     advice = `${color.name} works well as a foreground color on a ${rowColor.name} color background.`;
   }
   else if(contrastRatio >= 3.5){
-    advice = `${color.name} has a low contrast ratio on a ${rowColor.name} color background. Only use this for fonts larger than 24px or large block elements.`;
+    advice = `${color.name} has a low contrast ratio on a ${rowColor.name} color background. 
+    Only use this for fonts larger than 24px or large block elements.`;
   }
   else{
-    advice = `${color.name} is almost invisible on a ${rowColor.name} color background. Do not use this pairing.`;
+    advice = `${color.name} is almost invisible on a ${rowColor.name} color background. 
+    Do not use this pairing.`;
   }
 
   return (
     <div className="col-md-6 col-xl-3 col-xxxl-2 mb-4">
-      <div className="card colorPair h-100" style={{ backgroundColor: rowColor.hex, color: color.hex }}>
-        <div className="card-heder">
+      <div className="card colorPair h-100 mb-2" style={{ backgroundColor: rowColor.hex, color: color.hex }}>
+        {/* <div className="card-header">
           <ContrastRatioGauge value={contrastRatio} contrastColor={contrastColor}/>
-        </div>
+        </div> */}
         <div className="card-body">
+          <h1 className="display-2 fw-bold">{contrastRatio}</h1>
           <h3 className="card-title">{color.name}</h3>
           <p className="card-text">
             {advice}
           </p>
         </div>
-        <div className="card-footer d-flex flex-column justify-content-end">  
-          <p className="card-text d-flex justify-content-between mb-1">
-            WCAG AAA
-            <span className="badge">
-              {wcagAAA}
-            </span>
-          </p>
-          <p className="card-text d-flex justify-content-between">
-            WCAG AA
-            <span className="badge">
-              {wcagAA}
-            </span>
-          </p>
+        <div className="card-body d-flex flex-column justify-content-end">
+            <p className={`card-text mb-0 ${wcagAAA ? "" : "text-decoration-line-through"}`}>
+              WCAG AAA
+              {wcagAAA && <i className="bi bi-check-circle-fill ms-2"></i>}
+            </p>
+            <p className={`card-text ${wcagAA ? "" : "text-decoration-line-through"}`}>
+              WCAG AA
+              {wcagAA && <i className="bi bi-check-circle-fill ms-2"></i>}
+            </p>
         </div>
       </div>
     </div>
