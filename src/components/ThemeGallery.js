70ecd99aadcca315fd372dc7c14a3a1340f53254
Lev Popov <levpopov1@gmail.com>
2021-04-27 18:38:09 +0100

    comments out unused code for now

--- src/components/ThemeGallery.js
+++ src/components/ThemeGallery.js
@@ -2,32 +2,29 @@ import Theme from "./Theme";
 
 function ThemeGallery({collections, setTheme}) {
 
-  const openOverviweSection = () => {
-    let currentSection = document.querySelector('#vac #s00');
-    let overviewSection = document.querySelector('#vac #s0');
-    currentSection.parentNode.classList.remove("open");
-    currentSection.classList.remove("show");
-    overviewSection.parentNode.classList.add("open");
-    overviewSection.classList.add("show");
-  }
+  // const openOverviweSection = () => {
+  //   let currentSection = document.querySelector('#vac #s00');
+  //   let overviewSection = document.querySelector('#vac #s0');
+  //   currentSection.parentNode.classList.remove("open");
+  //   currentSection.classList.remove("show");
+  //   overviewSection.parentNode.classList.add("open");
+  //   overviewSection.classList.add("show");
+  // }
   
   const handleThemeChange = (id, collectionName) => {
     let currentCollection = collections.find(cl => cl.name === collectionName);
     let nextTheme = currentCollection.themes.find(item => item.id === id);
     setTheme(nextTheme);
-    openOverviweSection();
+    // openOverviweSection();
   }
 
   return (
-    <div className="section-container py-3">
-      <div className="row">
-        {collections.map(collection =>
-          // <ThemeCollection collection={collection} key={collection.id}/>
-          collection.themes.map(theme => 
-            <Theme theme={theme} setTheme={handleThemeChange} key={theme.id}/>
-          )
-        )}
-      </div>
+    <div className="row">
+      {collections.map(collection =>
+        collection.themes.map(theme => 
+          <Theme theme={theme} handleThemeChange={handleThemeChange} key={theme.id}/>
+        )
+      )}
     </div>
   );
 }
