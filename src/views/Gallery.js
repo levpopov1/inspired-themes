f7037d20f7269761b087c92076e82c6d2e2c7d70
Lev Popov <levpopov1@gmail.com>
2021-04-27 18:37:27 +0100

    adds themeGallery component

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,8 +1,10 @@
-function Gallery() {
+import ThemeGallery from "../components/ThemeGallery";
+
+function Gallery({collections, setTheme}) {
   return (
     <div className="router-view-gallery">
       <div className="container-fluid">
-        
+        <ThemeGallery collections={collections} setTheme={setTheme}/>
       </div>
     </div>
   );
