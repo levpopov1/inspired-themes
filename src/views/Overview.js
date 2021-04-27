a0fd85d5b43116335eff60d70583eae9bb545176
Lev Popov <levpopov1@gmail.com>
2021-04-27 18:58:49 +0100

    implements themeOverview component

--- src/views/Overview.js
+++ src/views/Overview.js
@@ -1,7 +1,16 @@
-function Overview() {
+import ThemeOverview from "../components/ThemeOverview";
+
+function Overview({theme}) {
+
+  let image = {
+    background: 'url(' + theme.image + ') center / cover no-repeat'
+  }
+
   return (
     <div className="router-view-overview">
-      <h1 className="title">Overview</h1>
+      <div className="container-fluid fullscreen d-flex flex-column justify-content-between" style={image}>
+        <ThemeOverview theme={theme}/>
+      </div>
     </div>
   );
 }
