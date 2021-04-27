015eaa62db64a2112a6f86ff21fa694748479bac
Lev Popov <levpopov1@gmail.com>
2021-04-27 19:50:32 +0100

    adds responsive classes

--- src/components/PaletteBlock.js
+++ src/components/PaletteBlock.js
@@ -1,22 +1,22 @@
 function PaletteBlock({color}) {
   return (
-    <div className="col">
-      <div className="palette-block mb-2" style={{ backgroundColor: color.hex }}></div>
-      <div className="colorData">
-        <h3 className="card-title text-center">{ color.name || "undefined" }</h3>
-        <p className="card-text text-center">
+    <div className="col-12 col-md d-flex d-md-block flex-row flex-md-column align-items-center justify-content-between">
+      <div className="palette-block mb-md-2" style={{ backgroundColor: color.hex }}></div>
+      <div className="colorData w-100 d-flex d-md-block flex-row flex-md-column align-items-center justify-content-between">
+        <h3 className="card-title text-center ms-2 ms-md-0">{ color.name || "undefined" }</h3>
+        <p className="card-text text-center d-block">
           <code className="code css d-block">
               { color.hex || "undefined" }
           </code>
         </p>
-        {/* <p className="card-text text-center d-none d-xl-block">
+        {/* <p className="card-text text-center d-none d-xxl-block">
           <code className="code css d-block">
               { color.rgb || "undefined" }
           </code>
         </p> */}
-        {/* <p className="card-text">
+        {/* <p className="card-text text-center d-none d-xxl-block">
           <code className="code css d-block">
-            { color.hsl }
+              { color.hsl || "undefined" }
           </code>
         </p> */}
       </div>
