84c5cc6798ece0c97ea8b4d65d1445f429e07c2a
Lev Popov <levpopov1@gmail.com>
2021-04-28 00:32:59 +0100

    creates header component

---
+++ src/components/ColorHeader.js
@@ -0,0 +1,12 @@
+import WCAGinfo from './WCAGinfo';
+
+function ColorHeader() {
+  return (
+    <div className="row">
+      <h2 className="title my-3">Pairings</h2>
+      <WCAGinfo/>
+    </div>
+  );
+}
+
+export default ColorHeader;
