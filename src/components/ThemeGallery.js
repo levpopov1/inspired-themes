da7805af47d14bf3d2b5c6abc780a240638a2816
Lev Popov <levpopov1@gmail.com>
2021-04-27 18:59:43 +0100

    removes unused code

--- src/components/ThemeGallery.js
+++ src/components/ThemeGallery.js
@@ -1,21 +1,11 @@
 import Theme from "./Theme";
 
 function ThemeGallery({collections, setTheme}) {
-
-  // const openOverviweSection = () => {
-  //   let currentSection = document.querySelector('#vac #s00');
-  //   let overviewSection = document.querySelector('#vac #s0');
-  //   currentSection.parentNode.classList.remove("open");
-  //   currentSection.classList.remove("show");
-  //   overviewSection.parentNode.classList.add("open");
-  //   overviewSection.classList.add("show");
-  // }
   
   const handleThemeChange = (id, collectionName) => {
     let currentCollection = collections.find(cl => cl.name === collectionName);
     let nextTheme = currentCollection.themes.find(item => item.id === id);
     setTheme(nextTheme);
-    // openOverviweSection();
   }
 
   return (
