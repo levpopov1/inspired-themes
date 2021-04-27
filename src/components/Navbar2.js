89147a8d31ac997887b6248742d6278c4efab8ae
Lev Popov <levpopov1@gmail.com>
2021-04-28 00:36:51 +0100

    handles menu collapse on click

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,6 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import InternalThemeSwither from './InternalThemeSwitcher';
 
+const closeMenu = () => {
+  let navCollapse = document.querySelector(".navbar-collapse");
+  navCollapse.classList.remove("show");
+}
+
 function NavBar2() {
   return (
     <nav className="navbar navbar-expand-lg navbar-adaptive sticky-top">
@@ -15,27 +20,27 @@ function NavBar2() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <NavLink to="/gallery" className="nav-link">
+              <NavLink to="/gallery" className="nav-link" onClick={closeMenu}>
                 Gallery
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/overview" className="nav-link">
+              <NavLink to="/overview" className="nav-link" onClick={closeMenu}>
                 Overview
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/color" className="nav-link">
+              <NavLink to="/color" className="nav-link" onClick={closeMenu}>
                 Color
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/typography" className="nav-link">
+              <NavLink to="/typography" className="nav-link" onClick={closeMenu}>
                 Typography
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/layout" className="nav-link">
+              <NavLink to="/layout" className="nav-link" onClick={closeMenu}>
                 Layout
               </NavLink>
             </li>
