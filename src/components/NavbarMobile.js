768faad598c04e361e4b9820f92d4cd812800dd8
Lev Popov <levpopov1@gmail.com>
2021-04-28 17:58:38 +0100

    fixes classnames

--- src/components/NavbarMobile.js
+++ src/components/NavbarMobile.js
@@ -6,27 +6,27 @@ function NavbarMobile() {
       <ul className="navbar-nav w-100 flex-row justify-content-around">
         <li className="nav-item">
           <NavLink to="/gallery" className="nav-link">
-            <i class="bi bi-collection"></i>
+            <i className="bi bi-collection"></i>
           </NavLink>
         </li>
         <li className="nav-item">
           <NavLink to="/overview" className="nav-link">
-            <i class="bi bi-image"></i>
+            <i className="bi bi-image"></i>
           </NavLink>
         </li>
         <li className="nav-item">
           <NavLink to="/color" className="nav-link">
-            <i class="bi bi-palette"></i>
+            <i className="bi bi-palette"></i>
           </NavLink>
         </li>
         <li className="nav-item">
           <NavLink to="/typography" className="nav-link">
-            <i class="bi bi-fonts"></i>
+            <i className="bi bi-fonts"></i>
           </NavLink>
         </li>
         <li className="nav-item">
           <NavLink to="/layout" className="nav-link">
-            <i class="bi bi-columns-gap"></i>
+            <i className="bi bi-columns-gap"></i>
           </NavLink>
         </li>
       </ul>
