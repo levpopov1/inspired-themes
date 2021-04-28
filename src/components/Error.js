425396eb40dc5829c7d231413b0ffa69b6694c80
Lev Popov <levpopov1@gmail.com>
2021-04-28 17:57:49 +0100

    Adds error component

---
+++ src/components/Error.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function Error({message, type}) {
+
+  const [visibility, setVisibility] = useState(true);
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="col-sm-4">
+        <div className={`card text-dark bg-${type ? type : "light"} my-3 ${visibility ? "d-block" : "d-none"}`}>
+          <div className="card-body d-flex flex-row justify-content-between">
+            <p className="card-text mb-0">
+              {message}
+            </p>
+            <button type="button" className="btn-close" aria-label="Close" onClick={() => setVisibility(false)}></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
