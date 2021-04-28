eb5cb4ead97e37b20137cd394d38458f258b8838
Lev Popov <levpopov1@gmail.com>
2021-04-28 23:32:56 +0100

    removes error component for now

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,14 +1,10 @@
 import ThemeGallery from "../components/ThemeGallery";
-import Error from "../components/Error";
 
 function Gallery({collections, setTheme}) {
   return (
     <div className="router-view-gallery">
       <div className="container-fluid">
-        {collections.length > 0 
-          ? <ThemeGallery collections={collections} setTheme={setTheme}/>
-          : <Error message={"No content found"} type={"warning"}/>
-        }
+        <ThemeGallery collections={collections} setTheme={setTheme}/>
       </div>
     </div>
   );
