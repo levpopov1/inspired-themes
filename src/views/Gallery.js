cf506d1268b789fa389b70234b79cd9e352cd3db
Lev Popov <levpopov1@gmail.com>
2021-04-28 17:58:12 +0100

    adds error message if no contnet

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,10 +1,14 @@
 import ThemeGallery from "../components/ThemeGallery";
+import Error from "../components/Error";
 
 function Gallery({collections, setTheme}) {
   return (
     <div className="router-view-gallery">
       <div className="container-fluid">
-        <ThemeGallery collections={collections} setTheme={setTheme}/>
+        {collections.length > 0 
+          ? <ThemeGallery collections={collections} setTheme={setTheme}/>
+          : <Error message={"No content found"} type={"warning"}/>
+        }
       </div>
     </div>
   );
