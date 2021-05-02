04d25300ea27d01d9ccf0371026697f3a811f75d
Lev Popov <levpopov1@gmail.com>
2021-05-02 18:52:21 +0100

    adds temporary description

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,7 +1,13 @@
 function Layout() {
   return (
     <div className="router-view-layout">
-      <h1 className="title">Layout</h1>
+      <div className="container-fluid">
+        <h1 className="title">Layout</h1>
+        <p className="text">
+          Coming soon. This section will show an example page layout, showcasing typical website components
+          such as buttons, CTAs, hero image, etc...
+        </p>
+      </div>
     </div>
   );
 }
