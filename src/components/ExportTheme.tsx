cf75209aa319b3a48c6d12fd6c00f1e61c324892
Lev Popov <levpopov1@gmail.com>
2021-05-21 19:20:38 +0100

    applies css export config

--- src/components/ExportTheme.tsx
+++ src/components/ExportTheme.tsx
@@ -1,31 +1,67 @@
 import React from 'react';
-import { iTheme } from '../interfaces';
+import { useState, useEffect, useRef } from 'react';
+import { iColor, iTheme } from '../interfaces';
+import exportConfig from '../lib/ExportThemeConfig';
+import Prism from 'prismjs';
+import '../styles/syntaxHighlighter.scss';
 
 interface Props {
   theme: iTheme
 }
 
 const ExportTheme: React.FC<Props> = ({theme}) => {
+
+  const [colorType, setColorType] = useState<keyof iColor>("hex");
+  const [cssCode, setCssCode] = useState("");
+  const codeRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    setCssCode(exportConfig(theme, colorType));
+  }, [colorType, theme]);
+
+  useEffect(() => {
+    if (codeRef && codeRef.current) {
+      Prism.highlightElement(codeRef.current);
+    }
+  });
+
   return (
     <div className="modal fade" id="exportThemeModal" tabIndex={-1} aria-labelledby="exportThemeModalLabel" aria-hidden="true">
-      <div className="modal-dialog">
+      <div className="modal-dialog modal-lg">
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title" id="exportThemeModalLabel">Modal title</h5>
+            <h5 className="modal-title text-muted" id="exportThemeModalLabel">Export Theme</h5>
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
-            <h3 className="title">
-              Coming Soon
-            </h3>
-            <p className="text">
-              This modal will allow users to export the current theme as CSS. 
-              Providing them with rules for all the colors, their chosen font type etc...
-            </p>
+            <div className="d-flex flex-column flex-md-row justify-content-md-between align-items-md-center mb-3">
+              <h3 className="title">
+                { theme.name }
+              </h3>
+              <div className="settings d-flex flex-row align-items-center">
+                <p className="modal-text me-3 mb-0">Color values:</p>
+                <div className="btn-group d-block" role="group">
+                <button type="button" className={`btn btn-outline ${colorType === "hex" ? "active" : ""}`} onClick={() => setColorType("hex")}>
+                  Hex
+                </button>
+                <button type="button" className={`btn btn-outline ${colorType === "rgb" ? "active" : ""}`} onClick={() => setColorType("rgb")}>
+                  RGB
+                </button>
+                <button type="button" className={`btn btn-outline ${colorType === "hsl" ? "active" : ""}`} onClick={() => setColorType("hsl")}>
+                  HSL
+                </button>
+              </div>
+              </div>
+            </div>
+            <pre>
+              <code className="language-css" ref={codeRef}>
+                { cssCode }
+              </code>
+            </pre>
           </div>
           <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary">Export</button>
+            <button type="button" className="btn btn-outline">Export .css file</button>
+            <button type="button" className="btn btn-primary">Copy to clipboard</button>
           </div>
         </div>
       </div>
